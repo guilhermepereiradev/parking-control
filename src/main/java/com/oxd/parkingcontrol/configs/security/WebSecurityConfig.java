65ff7197aaca444edb8cc74c1b09dee184ddc878
guilhermepereiradev <guilhermepereira1609@hotmail.com>
2022-09-12 15:10:51 -0300

    controle de acesso por roles

--- src/main/java/com/oxd/parkingcontrol/configs/security/WebSecurityConfig.java
+++ src/main/java/com/oxd/parkingcontrol/configs/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.oxd.parkingcontrol.configs.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,7 +22,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http.httpBasic()
                 .and()
-                .authorizeHttpRequests().anyRequest().authenticated()
+                .authorizeHttpRequests()
+                .antMatchers(HttpMethod.GET, "/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
     }
